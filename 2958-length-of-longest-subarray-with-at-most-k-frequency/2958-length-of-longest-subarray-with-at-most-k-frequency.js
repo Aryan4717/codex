e48c7021a9318108879567ef6319cc0498a87b31
Aryan4717 <ak1710650@gmail.com>
2026-08-12 22:38:39 +0530

    Time: 60 ms (51.02%), Space: 72.9 MB (83.67%) - LeetHub

---
+++ 2958-length-of-longest-subarray-with-at-most-k-frequency/2958-length-of-longest-subarray-with-at-most-k-frequency.js
@@ -0,0 +1,18 @@
+var maxSubarrayLength = function(nums, k) {
+    const freq = new Map();
+    let left = 0;
+    let ans = 0;
+
+    for (let right = 0; right < nums.length; right++) {
+        freq.set(nums[right], (freq.get(nums[right]) || 0) + 1);
+
+        while (freq.get(nums[right]) > k) {
+            freq.set(nums[left], freq.get(nums[left]) - 1);
+            left++;
+        }
+
+        ans = Math.max(ans, right - left + 1);
+    }
+
+    return ans;
+};
