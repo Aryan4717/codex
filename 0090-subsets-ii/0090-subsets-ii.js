8b3850ea662f7c6e684c9bf10272668bb058bc79
Aryan4717 <ak1710650@gmail.com>
2026-08-18 18:14:32 +0530

    Time: 0 ms (100%), Space: 58.1 MB (91.75%) - LeetHub

---
+++ 0090-subsets-ii/0090-subsets-ii.js
@@ -0,0 +1,20 @@
+var subsetsWithDup = function(nums) {
+    nums.sort((a, b) => a - b);
+    const res = [];
+    const subset = [];
+
+    const backtrack = (start) => {
+        res.push([...subset]);
+
+        for (let i = start; i < nums.length; i++) {
+            if (i > start && nums[i] === nums[i - 1]) continue;
+
+            subset.push(nums[i]);
+            backtrack(i + 1);
+            subset.pop();
+        }
+    };
+
+    backtrack(0);
+    return res;
+};
